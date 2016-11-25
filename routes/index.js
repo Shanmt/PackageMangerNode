c5102ac4b8e97addbff0f83b34af788ec5ab87ae
Tigris <Tigris>
2016-11-25 17:48:30 +0530

    Latest Commit

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,38 @@
 var express = require('express');
+var url = require('url');
 var router = express.Router();
-var mongo = require('mongodb'),  
-  Server = mongo.Server,
-  Db = mongo.Db;
-var server = new Server('localhost', 27017, {  
-  auto_reconnect: true
-});
-var db = new Db('PackageManager', server);  
-var onErr = function(err, callback) {  
-  db.close();
-  callback(err);
-};
+var MongoClient = require('mongodb').MongoClient,
+ assert = require('assert');
  
+// Connection URL 
+var url = 'mongodb://localhost:27017/PackageManager';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.html');
+    //res.render('index.html');
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+       
+        
+        var collection = db.collection('users');
+        
+        emailid = req.query.email;
+        password = req.query.password;
+        collection.find({"email":emailid,"password":password}).toArray(function(err, docs) {
+            if(docs.length > 0){
+                res.end(JSON.stringify(docs));
+            }
+            else{
+                res.end(JSON.stringify("No User"))
+            }
+        });
+
+        db.close();
+       
+     });
+
     res.end();
 });
  
